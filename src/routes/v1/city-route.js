9ce5f9ee8cbba3bfef60be81a61b77ca5831da09
Ritik verma <vermaritik933@gmail.com>
2025-01-22 22:54:04 +0530

    ✨ Add City middlewares for request validation in city routes

--- src/routes/v1/city-route.js
+++ src/routes/v1/city-route.js
@@ -1,9 +1,9 @@
 const exprees = require('express');
 const router = exprees.Router();
 const { CityController } = require('../../controllers');
+const { CityMiddlewares } = require('../../middlewares');
 
 // /api/v1/cities POST
-router.post('/', CityController.createCity);
-
+router.post('/', CityMiddlewares.validateCreateRequest, CityController.createCity);
 
 module.exports = router;
